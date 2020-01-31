9b3584a0c2a9a89cd7d0bcc1208428fa4e71a3f7
Kai Volland <kaivolland@yahoo.de>
2020-01-31 13:34:11 +0100

    Adds graphql stuff for Layer and Application

--- shogun-lib/src/main/java/de/terrestris/shoguncore/graphql/GraphQLDataFetchers.java
+++ shogun-lib/src/main/java/de/terrestris/shoguncore/graphql/GraphQLDataFetchers.java
@@ -1,6 +1,10 @@
 package de.terrestris.shoguncore.graphql;
 
+import de.terrestris.shoguncore.model.Application;
+import de.terrestris.shoguncore.model.Layer;
 import de.terrestris.shoguncore.model.User;
+import de.terrestris.shoguncore.repository.ApplicationRepository;
+import de.terrestris.shoguncore.repository.LayerRepository;
 import de.terrestris.shoguncore.repository.UserRepository;
 import graphql.schema.DataFetcher;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,7 +16,43 @@ import java.util.Optional;
 public class GraphQLDataFetchers {
 
     @Autowired
+    ApplicationRepository applicationRepository;
+
+    @Autowired
+    LayerRepository layerRepository;
+
+    @Autowired
     UserRepository userRepository;
+
+    public DataFetcher getApplicationById() {
+        return dataFetchingEnvironment -> {
+            Long applicationId = Long.parseLong(dataFetchingEnvironment.getArgument("id"));
+            Optional<Application> application = this.applicationRepository.findById(applicationId);
+            return application;
+        };
+    }
+
+    public DataFetcher getAllApplications() {
+        return dataFetchingEnvironment -> {
+            Iterable<Application> applications = this.applicationRepository.findAll();
+            return applications;
+        };
+    }
+
+    public DataFetcher getLayerById() {
+        return dataFetchingEnvironment -> {
+            Long layerId = Long.parseLong(dataFetchingEnvironment.getArgument("id"));
+            Optional<Layer> layer = this.layerRepository.findById(layerId);
+            return layer;
+        };
+    }
+
+    public DataFetcher getAllLayers() {
+        return dataFetchingEnvironment -> {
+            Iterable<Layer> layers = this.layerRepository.findAll();
+            return layers;
+        };
+    }
 
     public DataFetcher getUserById() {
         return dataFetchingEnvironment -> {
