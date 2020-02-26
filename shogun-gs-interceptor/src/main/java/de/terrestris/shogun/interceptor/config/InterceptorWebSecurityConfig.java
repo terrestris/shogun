b4aeb58a8287d303fc47af4856699e0d3917c5bd
Daniel Koch <koch@terrestris.de>
2020-02-26 09:51:54 +0100

    :lipstick:

--- shogun-gs-interceptor/src/main/java/de/terrestris/shogun/interceptor/config/InterceptorWebSecurityConfig.java
+++ shogun-gs-interceptor/src/main/java/de/terrestris/shogun/interceptor/config/InterceptorWebSecurityConfig.java
@@ -3,24 +3,25 @@ package de.terrestris.shogun.interceptor.config;
 import de.terrestris.shogun.config.WebSecurityConfig;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configurers.ExpressionUrlAuthorizationConfigurer;
 
 @Configuration
 public class InterceptorWebSecurityConfig extends WebSecurityConfig {
 
     @Override
-    protected ExpressionUrlAuthorizationConfigurer<HttpSecurity>.ExpressionInterceptUrlRegistry customHttpConfiguration(HttpSecurity http) throws Exception {
-        return http.authorizeRequests()
-            .antMatchers(
-                // Allow access to swagger interface
-                "/swagger-ui.html",
-                "/swagger-resources/**",
-                "/webjars/**",
-                "/v2/**",
-                "/csrf/**"
-            )
-            .permitAll()
-            .anyRequest()
-            .hasRole("user");
+    protected void customHttpConfiguration(HttpSecurity http) throws Exception {
+        http
+            .authorizeRequests()
+                .antMatchers(
+                    // Allow access to swagger interface
+                    "/swagger-ui.html",
+                    "/swagger-resources/**",
+                    "/webjars/**",
+                    "/v2/**",
+                    "/csrf/**"
+                )
+                    .permitAll()
+                .anyRequest()
+                    // .authenticated()
+                    .hasRole("user");
     }
 }
