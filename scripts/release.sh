8be9838fbf812209f10856bc934708854b5285ae
Daniel Koch <koch@terrestris.de>
2020-03-04 17:40:51 +0100

    Check for JAVA_HOME

--- scripts/release.sh
+++ scripts/release.sh
@@ -24,6 +24,12 @@ if [[ ! $DEVELOPMENT_VERSION =~ ^([0-9]+\.[0-9]+\.[0-9])(\-SNAPSHOT)$ ]]; then
     exit 1
 fi
 
+# Check if JAVA_HOME is set
+if [[ -z $JAVA_HOME ]]; then
+    echo "Error: JAVA_HOME is not set"
+    exit 1
+fi
+
 SCRIPTDIR=$(dirname "$0")
 
 pushd "$SCRIPTDIR"/..
