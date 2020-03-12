8d3f4e75c67058d114a9af40d007bc6905465999
Andre Henn <henn@terrestris.de>
2020-03-12 12:36:53 +0100

    adds keycloak user / group handling to util

--- shogun-lib/src/main/java/de/terrestris/shogun/lib/listener/LoginListener.java
+++ shogun-lib/src/main/java/de/terrestris/shogun/lib/listener/LoginListener.java
@@ -4,6 +4,7 @@ import de.terrestris.shogun.lib.model.Group;
 import de.terrestris.shogun.lib.model.User;
 import de.terrestris.shogun.lib.repository.GroupRepository;
 import de.terrestris.shogun.lib.repository.UserRepository;
+import de.terrestris.shogun.lib.security.SecurityContextUtil;
 import org.jboss.resteasy.client.jaxrs.ResteasyClient;
 import org.jboss.resteasy.client.jaxrs.ResteasyClientBuilder;
 import org.keycloak.KeycloakPrincipal;
@@ -23,34 +24,14 @@ import java.util.List;
 
 @Component
 public class LoginListener implements ApplicationListener<InteractiveAuthenticationSuccessEvent> {
-
-    private Keycloak keycloak;
-
-    private RealmResource realm;
+    @Autowired
+    protected SecurityContextUtil securityContextUtil;
 
     @Autowired
     protected UserRepository userRepository;
 
     @Autowired
     protected GroupRepository groupRepository;
-
-    public LoginListener() {
-        // TODO: create beans for this
-        ResteasyClient restClient = new ResteasyClientBuilder()
-            .hostnameVerification(ResteasyClientBuilder.HostnameVerificationPolicy.ANY)
-            .build();
-
-        this.keycloak = KeycloakBuilder.builder()
-            .serverUrl("http://localhost:8000/auth")
-            .realm("master")
-            .username("admin")
-            .password("shogun")
-            .clientId("admin-cli")
-            .resteasyClient(restClient)
-            .build();
-
-        this.realm = keycloak.realm("SpringBootKeycloak");
-    }
 
     @Override
     public void onApplicationEvent(InteractiveAuthenticationSuccessEvent event)
@@ -70,22 +51,16 @@ public class LoginListener implements ApplicationListener<InteractiveAuthenticat
         // add missing user to shogun db
         User user = userRepository.findByKeycloakId(keycloakUserId);
         if (user == null) {
-            User newUser = new User();
-            newUser.setKeycloakId(keycloakUserId);
+            User newUser = new User(keycloakUserId, null, null);
             userRepository.save(newUser);
         }
 
-        // get user groups from keycloak
-        UsersResource ur = realm.users();
-        UserResource kcUser = ur.get(keycloakUserId);
-        List<GroupRepresentation> userGroups = kcUser.groups();
-
+        List<GroupRepresentation> userGroups = securityContextUtil.getKeycloakGroupsForUser(user);
         // add missing groups to shogun db
         userGroups.stream().map(GroupRepresentation::getId).forEach(keycloakGroupId -> {
             Group group = groupRepository.findByKeycloakId(keycloakGroupId);
             if (group == null) {
-                Group newGroup = new Group();
-                newGroup.setKeycloakId(keycloakGroupId);
+                Group newGroup = new Group(keycloakGroupId);
                 groupRepository.save(newGroup);
             }
         });
--- shogun-lib/src/main/java/de/terrestris/shogun/lib/security/SecurityContextUtil.java
+++ shogun-lib/src/main/java/de/terrestris/shogun/lib/security/SecurityContextUtil.java
@@ -1,10 +1,20 @@
 package de.terrestris.shogun.lib.security;
 
+import de.terrestris.shogun.lib.model.Group;
 import de.terrestris.shogun.lib.model.User;
+import de.terrestris.shogun.lib.repository.GroupRepository;
 import de.terrestris.shogun.lib.repository.UserRepository;
 import de.terrestris.shogun.lib.specification.UserSpecification;
+import org.jboss.resteasy.client.jaxrs.ResteasyClient;
+import org.jboss.resteasy.client.jaxrs.ResteasyClientBuilder;
 import org.keycloak.KeycloakPrincipal;
+import org.keycloak.admin.client.Keycloak;
+import org.keycloak.admin.client.KeycloakBuilder;
+import org.keycloak.admin.client.resource.RealmResource;
+import org.keycloak.admin.client.resource.UserResource;
+import org.keycloak.representations.idm.GroupRepresentation;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
@@ -13,12 +23,36 @@ import org.springframework.transaction.annotation.Transactional;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 @Component
 public class SecurityContextUtil {
 
+    private RealmResource realm;
+
     @Autowired
-    UserRepository userRepository;
+    private UserRepository userRepository;
+
+    @Autowired
+    private GroupRepository groupRepository;
+
+    public SecurityContextUtil() {
+        // TODO: create beans for this
+        ResteasyClient restClient = new ResteasyClientBuilder()
+            .hostnameVerification(ResteasyClientBuilder.HostnameVerificationPolicy.ANY)
+            .build();
+
+        Keycloak keycloak = KeycloakBuilder.builder()
+            .serverUrl("http://localhost:8000/auth")
+            .realm("master")
+            .username("admin")
+            .password("shogun")
+            .clientId("admin-cli")
+            .resteasyClient(restClient)
+            .build();
+
+        this.realm = keycloak.realm("SpringBootKeycloak");
+    }
 
     @Transactional(readOnly = true)
     public Optional<User> getUserBySession() {
@@ -49,5 +83,49 @@ public class SecurityContextUtil {
 
         List<GrantedAuthority> grantedAuthorities = new ArrayList<>();
         return grantedAuthorities;
+    }
+
+    /**
+     * Returns the current user object from the database.
+     *
+     * @param authentication
+     * @return
+     */
+    public User getUserFromAuthentication(Authentication authentication) {
+        final Object principal = authentication.getPrincipal();
+        if (!(principal instanceof KeycloakPrincipal)) {
+            return null;
+        }
+        KeycloakPrincipal<?> keycloakPrincipal = (KeycloakPrincipal<?>) principal;
+        String keycloakId = keycloakPrincipal.getKeycloakSecurityContext().getIdToken().getSubject();
+        return userRepository.findByKeycloakId(keycloakId);
+    }
+
+    /**
+     * Get (SHOGun) groups for user based on actual assignment in keycloak
+     * @param user The SHOGun user
+     * @return List of groups
+     */
+    public List<Group> getGroupsForUser(User user) {
+        List<GroupRepresentation> userGroups = this.getKeycloakGroupsForUser(user);
+        if (userGroups == null) {
+            return null;
+        }
+
+        // return list of Groups that are in SHOGun DB
+        return userGroups.stream().
+            map(GroupRepresentation::getId).
+            map(keycloakGroupId -> groupRepository.findByKeycloakId(keycloakGroupId)).
+            collect(Collectors.toList());
+    }
+
+    /**
+     * Return keycloak GroupRepresentaions (groups) for user
+     * @param user
+     * @return
+     */
+    public List<GroupRepresentation> getKeycloakGroupsForUser(User user) {
+        UserResource kcUser = this.realm.users().get(user.getKeycloakId());
+        return kcUser != null ? kcUser.groups() : null;
     }
 }
