f94d31e4d0bace911a367ee77f0a2951323e222c
Andre Henn <henn@terrestris.de>
2020-03-20 13:41:21 +0100

    remove InterceptorRuleSpecification

--- shogun-gs-interceptor/src/main/java/de/terrestris/shogun/interceptor/repository/InterceptorRuleRepository.java
+++ shogun-gs-interceptor/src/main/java/de/terrestris/shogun/interceptor/repository/InterceptorRuleRepository.java
@@ -1,10 +1,17 @@
 package de.terrestris.shogun.interceptor.repository;
 
+import de.terrestris.shogun.interceptor.enumeration.HttpEnum;
+import de.terrestris.shogun.interceptor.enumeration.OgcEnum;
 import de.terrestris.shogun.interceptor.model.InterceptorRule;
 import de.terrestris.shogun.lib.repository.BaseCrudRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface InterceptorRuleRepository extends BaseCrudRepository<InterceptorRule, Long>, JpaSpecificationExecutor<InterceptorRule> {
+
+    List<InterceptorRule> findAllByServiceAndEvent(OgcEnum.ServiceType service, HttpEnum.EventType event);
+
 }
--- shogun-gs-interceptor/src/main/java/de/terrestris/shogun/interceptor/service/InterceptorRuleService.java
+++ shogun-gs-interceptor/src/main/java/de/terrestris/shogun/interceptor/service/InterceptorRuleService.java
@@ -4,7 +4,6 @@ import de.terrestris.shogun.interceptor.enumeration.HttpEnum;
 import de.terrestris.shogun.interceptor.enumeration.OgcEnum;
 import de.terrestris.shogun.interceptor.model.InterceptorRule;
 import de.terrestris.shogun.interceptor.repository.InterceptorRuleRepository;
-import de.terrestris.shogun.interceptor.specification.InterceptorRuleSpecification;
 import de.terrestris.shogun.lib.service.BaseService;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -16,7 +15,31 @@ public class InterceptorRuleService extends BaseService<InterceptorRuleRepositor
 
     @Transactional(readOnly = true)
     public List<InterceptorRule> findAllRulesForServiceAndEvent(OgcEnum.ServiceType service, HttpEnum.EventType event) {
-        return repository.findAll(InterceptorRuleSpecification.findAllByServiceAndEvent(service, event));
+        return repository.findAllByServiceAndEvent(service, event);
+    }
+
+    public void addRequestRule() {
+
+    }
+
+    public void addResponseRule() {
+
+    }
+
+    public void addRule() {
+
+    }
+
+    public void removeRequestRule() {
+
+    }
+
+    public void removeResponseRule() {
+
+    }
+
+    public void removeRule() {
+
     }
 
 }
