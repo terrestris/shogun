26058fa13a377b1db4e87cb47bcfe3049f1b399c
Andre Henn <henn@terrestris.de>
2020-04-03 15:58:40 +0200

    make precision model and srd of JTS module configurable

--- shogun-lib/src/main/java/de/terrestris/shogun/lib/config/JacksonConfig.java
+++ shogun-lib/src/main/java/de/terrestris/shogun/lib/config/JacksonConfig.java
@@ -3,16 +3,22 @@ package de.terrestris.shogun.lib.config;
 import com.bedatadriven.jackson.datatype.jts.JtsModule;
 import org.locationtech.jts.geom.GeometryFactory;
 import org.locationtech.jts.geom.PrecisionModel;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class JacksonConfig {
 
+    @Value("${shogun.srid:4326}")
+    protected int srid;
+
+    @Value("${shogun.coordinatePrecisionScale:10}")
+    protected int coordinatePrecisionScale;
+
     @Bean
     public JtsModule jtsModule() {
-        GeometryFactory geomFactory = new GeometryFactory(new PrecisionModel(10), 4326);
-
+        GeometryFactory geomFactory = new GeometryFactory(new PrecisionModel(coordinatePrecisionScale), srid);
         return new JtsModule(geomFactory);
     }
 
