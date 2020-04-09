f96d4601438d42e166f7332e0d07c3e4f597a0bb
Kai Volland <kaivolland@yahoo.de>
2020-04-09 12:52:10 +0200

    Replaces repositories with services in DataFetcher

--- shogun-lib/src/main/java/de/terrestris/shogun/lib/graphql/GraphQLDataFetchers.java
+++ shogun-lib/src/main/java/de/terrestris/shogun/lib/graphql/GraphQLDataFetchers.java
@@ -3,6 +3,9 @@ package de.terrestris.shogun.lib.graphql;
 import de.terrestris.shogun.lib.repository.ApplicationRepository;
 import de.terrestris.shogun.lib.repository.LayerRepository;
 import de.terrestris.shogun.lib.repository.UserRepository;
+import de.terrestris.shogun.lib.service.ApplicationService;
+import de.terrestris.shogun.lib.service.LayerService;
+import de.terrestris.shogun.lib.service.UserService;
 import graphql.schema.DataFetcher;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
@@ -11,44 +14,44 @@ import org.springframework.stereotype.Component;
 public class GraphQLDataFetchers {
 
     @Autowired
-    ApplicationRepository applicationRepository;
+    ApplicationService applicationService;
 
     @Autowired
-    LayerRepository layerRepository;
+    LayerService layerService;
 
     @Autowired
-    UserRepository userRepository;
+    UserService userService;
 
     public DataFetcher getApplicationById() {
         return dataFetchingEnvironment -> {
             Long applicationId = Long.parseLong(dataFetchingEnvironment.getArgument("id"));
-            return this.applicationRepository.findById(applicationId);
+            return this.applicationService.findOne(applicationId);
         };
     }
 
     public DataFetcher getAllApplications() {
-        return dataFetchingEnvironment -> this.applicationRepository.findAll();
+        return dataFetchingEnvironment -> this.applicationService.findAll();
     }
 
     public DataFetcher getLayerById() {
         return dataFetchingEnvironment -> {
             Long layerId = Long.parseLong(dataFetchingEnvironment.getArgument("id"));
-            return this.layerRepository.findById(layerId);
+            return this.layerService.findOne(layerId);
         };
     }
 
     public DataFetcher getAllLayers() {
-        return dataFetchingEnvironment -> this.layerRepository.findAll();
+        return dataFetchingEnvironment -> this.layerService.findAll();
     }
 
     public DataFetcher getUserById() {
         return dataFetchingEnvironment -> {
             Long userId = Long.parseLong(dataFetchingEnvironment.getArgument("id"));
-            return this.userRepository.findById(userId);
+            return this.userService.findOne(userId);
         };
     }
 
     public DataFetcher getAllUsers() {
-        return dataFetchingEnvironment -> this.userRepository.findAll();
+        return dataFetchingEnvironment -> this.userService.findAll();
     }
 }
