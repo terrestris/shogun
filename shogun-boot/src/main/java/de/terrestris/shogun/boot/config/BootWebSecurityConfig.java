790492fed15e381af2061f2ac4c237ab4f49d4ef
Daniel Koch <koch@terrestris.de>
2020-06-02 11:46:26 +0200

    Disable CSRF protection for swagger-ui

--- shogun-boot/src/main/java/de/terrestris/shogun/boot/config/BootWebSecurityConfig.java
+++ shogun-boot/src/main/java/de/terrestris/shogun/boot/config/BootWebSecurityConfig.java
@@ -5,10 +5,25 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
+import org.springframework.security.web.util.matcher.RequestMatcher;
+
+import javax.servlet.http.HttpServletRequest;
 
 @Configuration
 @EnableWebSecurity
 public class BootWebSecurityConfig extends WebSecurityConfig {
+
+    RequestMatcher csrfRequestMatcher = new RequestMatcher() {
+        public boolean matches(HttpServletRequest httpServletRequest) {
+            String refererHeader = httpServletRequest.getHeader("Referer");
+
+            if (refererHeader != null && refererHeader.endsWith("swagger-ui.html")) {
+                return true;
+            }
+
+            return false;
+        }
+    };
 
     @Override
     protected void customHttpConfiguration(HttpSecurity http) throws Exception {
@@ -37,6 +52,7 @@ public class BootWebSecurityConfig extends WebSecurityConfig {
             .and()
                 .csrf()
                     .csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse())
+                    .ignoringRequestMatchers(csrfRequestMatcher)
                     .ignoringAntMatchers("/graphql");
     }
 
