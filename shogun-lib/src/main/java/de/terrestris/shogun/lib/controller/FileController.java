a351aaae8873b12705ec77c6cbc172fa7fdd2694
Daniel Koch <koch@terrestris.de>
2020-06-03 17:40:00 +0200

    Make use of a common BaseFileRepository, BaseFileService and BaseFileController for the File and ImageFile entities

--- shogun-lib/src/main/java/de/terrestris/shogun/lib/controller/FileController.java
+++ shogun-lib/src/main/java/de/terrestris/shogun/lib/controller/FileController.java
@@ -2,87 +2,13 @@ package de.terrestris.shogun.lib.controller;
 
 import de.terrestris.shogun.lib.model.File;
 import de.terrestris.shogun.lib.service.FileService;
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.MessageSource;
 import org.springframework.context.i18n.LocaleContextHolder;
-import org.springframework.http.*;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.server.ResponseStatusException;
 
-import java.util.UUID;
-
 @RestController
 @RequestMapping("/files")
-public class FileController {
-
-    protected final Logger LOG = LogManager.getLogger(getClass());
-
-    @Autowired
-    private FileService service;
-
-    @Autowired
-    protected MessageSource messageSource;
-
-    @PostMapping
-    @ResponseStatus(HttpStatus.CREATED)
-    public File add(@RequestParam("file") MultipartFile uploadedFile) {
-
-        LOG.debug("Requested to upload a multipart-file");
-
-        try {
-            File file = this.service.uploadFile(uploadedFile);
-
-            LOG.info("Successfully uploaded file " + file.getFileName());
-
-            return file;
-        } catch (Exception e) {
-            LOG.error("Could not upload the file: " + e.getMessage());
-            LOG.trace("Full stack trace: ", e);
-
-            throw new ResponseStatusException(
-                HttpStatus.INTERNAL_SERVER_ERROR,
-                messageSource.getMessage(
-                    "BaseController.INTERNAL_SERVER_ERROR",
-                    null,
-                    LocaleContextHolder.getLocale()
-                )
-            );
-        }
-    }
-
-    @GetMapping("/{fileUuid}")
-    @ResponseStatus(HttpStatus.OK)
-    public ResponseEntity<?> findOne(@PathVariable("fileUuid") UUID fileUuid) {
-
-        LOG.debug("Requested to return file with UUID {}", fileUuid);
-
-        try {
-            final HttpHeaders responseHeaders = new HttpHeaders();
-
-            File file = this.service.getFile(fileUuid);
-
-            LOG.info("Successfully got file with UUID {}", fileUuid);
-
-            responseHeaders.setContentType(MediaType.parseMediaType(file.getFileType()));
-            responseHeaders.setContentDisposition(ContentDisposition.parse(
-                String.format("inline; filename=\"%s\"", file.getFileName())));
-
-            return new ResponseEntity<>(file.getFile(), responseHeaders, HttpStatus.OK);
-        } catch (Exception e) {
-            LOG.error("Error while returning the file {}: ",  e.getMessage());
-            LOG.trace("Full stack trace: ", e);
-
-            throw new ResponseStatusException(
-                HttpStatus.INTERNAL_SERVER_ERROR,
-                messageSource.getMessage(
-                    "BaseController.INTERNAL_SERVER_ERROR",
-                    null,
-                    LocaleContextHolder.getLocale()
-                )
-            );
-        }
-    }
-}
+public class FileController extends BaseFileController<FileService, File> { }
--- shogun-lib/src/main/java/de/terrestris/shogun/lib/service/FileService.java
+++ shogun-lib/src/main/java/de/terrestris/shogun/lib/service/FileService.java
@@ -1,57 +1,41 @@
 package de.terrestris.shogun.lib.service;
 
-import de.terrestris.shogun.lib.repository.FileRepository;
 import de.terrestris.shogun.lib.model.File;
-import javassist.NotFoundException;
-import org.apache.commons.io.IOUtils;
-import org.springframework.security.access.prepost.PreAuthorize;
+import de.terrestris.shogun.lib.repository.FileRepository;
+import de.terrestris.shogun.lib.util.FileUtil;
+import de.terrestris.shogun.properties.UploadProperties;
+import org.apache.tomcat.util.http.fileupload.impl.InvalidContentTypeException;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
-import java.io.InputStream;
-import java.util.Optional;
-import java.util.UUID;
-
 @Service
-public class FileService extends BaseService<FileRepository, File> {
+public class FileService extends BaseFileService<FileRepository, File> {
 
-    @PreAuthorize("isAuthenticated()")
-    public File uploadFile(MultipartFile file) throws Exception {
+    @Autowired
+    private UploadProperties uploadProperties;
 
-        if (file == null) {
-            throw new Exception("Upload failed. File is null.");
-        } else if (file.isEmpty()) {
-            throw new Exception("Upload failed. File is empty.");
-        }
+    public File create(MultipartFile uploadFile) throws Exception {
 
-        byte[] fileByteArray = null;
-        File fileToPersist = new File();
+        FileUtil.validateFile(uploadFile);
 
-        try (InputStream is = file.getInputStream()) {
-            fileByteArray = IOUtils.toByteArray(is);
-        } catch (Exception e) {
-            throw new Exception("Could not create the bytearray: {}" + e.getMessage());
-        }
+        byte[] fileByteArray = FileUtil.fileToByteArray(uploadFile);
 
-        fileToPersist.setFile(fileByteArray);
-        fileToPersist.setFileType(file.getContentType());
-        fileToPersist.setFileName(file.getOriginalFilename());
-        fileToPersist.setActive(true);
+        File file = new File();
+        file.setFile(fileByteArray);
+        file.setFileType(uploadFile.getContentType());
+        file.setFileName(uploadFile.getOriginalFilename());
+        file.setActive(true);
 
-        this.repository.save(fileToPersist);
+        File savedFile = this.create(file);
 
-        return fileToPersist;
+        return savedFile;
     }
 
-    @PreAuthorize("isAuthenticated()")
-    public File getFile (UUID fileUuid) throws NotFoundException {
-        Optional<File> file = this.repository.findByFileUuid(fileUuid);
-
-        if (file.isPresent()) {
-            return file.get();
-        } else {
-            throw new NotFoundException("Could not find file by UUID " + fileUuid);
+    public void isValidType(String contentType) throws InvalidContentTypeException {
+        if (!uploadProperties.getFile().getSupportedContentTypes().contains(contentType)) {
+            throw new InvalidContentTypeException("Unsupported content type for upload!");
         }
-
     }
+
 }
