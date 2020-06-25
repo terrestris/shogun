d6ddbda01dc2ce9450d2381463146dbc3098dbe5
Daniel Koch <koch@terrestris.de>
2020-06-25 10:34:04 +0200

    Extend RevisionRepository to access entity revisions

--- shogun-lib/src/main/java/de/terrestris/shogun/lib/repository/BaseCrudRepository.java
+++ shogun-lib/src/main/java/de/terrestris/shogun/lib/repository/BaseCrudRepository.java
@@ -2,6 +2,7 @@ package de.terrestris.shogun.lib.repository;
 
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.NoRepositoryBean;
+import org.springframework.data.repository.history.RevisionRepository;
 
 @NoRepositoryBean
-public interface BaseCrudRepository<T, ID> extends CrudRepository<T, ID> { }
+public interface BaseCrudRepository<T, ID> extends RevisionRepository<T, ID, Integer>, CrudRepository<T, ID> { }
