555c10e75f1a12b5c78b938e276974d0a28e39d3
Daniel Koch <koch@terrestris.de>
2020-08-05 17:48:08 +0200

    Init ImageFileIdResolver

---
+++ shogun-lib/src/main/java/de/terrestris/shogun/lib/resolver/ImageFileIdResolver.java
@@ -0,0 +1,6 @@
+package de.terrestris.shogun.lib.resolver;
+
+import de.terrestris.shogun.lib.model.ImageFile;
+import de.terrestris.shogun.lib.service.ImageFileService;
+
+public class ImageFileIdResolver extends BaseEntityIdResolver<ImageFile, ImageFileService> { }
