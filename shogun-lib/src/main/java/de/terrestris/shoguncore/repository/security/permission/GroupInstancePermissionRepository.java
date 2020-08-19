80759148969b58edff7aed3b9cc238c61d06ed24
Daniel Koch <koch@terrestris.de>
2020-08-19 12:06:24 +0200

    Add method findAllByEntityId()

--- shogun-lib/src/main/java/de/terrestris/shoguncore/repository/security/permission/GroupInstancePermissionRepository.java
+++ shogun-lib/src/main/java/de/terrestris/shoguncore/repository/security/permission/GroupInstancePermissionRepository.java
@@ -5,6 +5,11 @@ import de.terrestris.shoguncore.repository.BaseCrudRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface GroupInstancePermissionRepository extends BaseCrudRepository<GroupInstancePermission, Long>, JpaSpecificationExecutor<GroupInstancePermission> {
+
+    List<GroupInstancePermission> findAllByEntityId(Long entityId);
+
 }
