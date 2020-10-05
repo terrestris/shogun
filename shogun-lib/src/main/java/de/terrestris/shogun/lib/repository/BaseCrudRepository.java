aa39c7279d508a32a62d29a04df2002644e1dbc0
Daniel Koch <koch@terrestris.de>
2020-10-05 17:27:01 +0200

    Make findAll cacheable

--- shogun-lib/src/main/java/de/terrestris/shogun/lib/repository/BaseCrudRepository.java
+++ shogun-lib/src/main/java/de/terrestris/shogun/lib/repository/BaseCrudRepository.java
@@ -1,8 +1,16 @@
 package de.terrestris.shogun.lib.repository;
 
+import java.util.List;
+import javax.persistence.QueryHint;
+import org.springframework.data.jpa.repository.QueryHints;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.NoRepositoryBean;
 import org.springframework.data.repository.history.RevisionRepository;
 
 @NoRepositoryBean
-public interface BaseCrudRepository<T, ID> extends RevisionRepository<T, ID, Integer>, CrudRepository<T, ID> { }
+public interface BaseCrudRepository<T, ID> extends RevisionRepository<T, ID, Integer>, CrudRepository<T, ID> {
+
+    @QueryHints(@QueryHint(name = org.hibernate.annotations.QueryHints.CACHEABLE, value = "true"))
+    List<T> findAll();
+
+}
