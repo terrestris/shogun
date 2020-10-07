0e68f0c4fa54af38923f8f103f752d7570df97e6
Daniel Koch <koch@terrestris.de>
2020-10-07 16:33:48 +0200

    Don't wrap java args in quotes to prevent unrecognized option error

--- shogun-gs-interceptor/docker/docker-entrypoint.sh
+++ shogun-gs-interceptor/docker/docker-entrypoint.sh
@@ -5,6 +5,6 @@ if [ x"${JAVA_ENABLE_DEBUG}" != x ] && [ "${JAVA_ENABLE_DEBUG}" != "false" ]; th
   JAVA_DEBUG_ARGS="-Xdebug -agentlib:jdwp=transport=dt_socket,server=y,suspend=n,address=${JAVA_DEBUG_PORT:-5005}"
 fi
 
-java "$JAVA_DEBUG_ARGS" -Djava.security.egd=file:/dev/./urandom -jar /opt/app.jar --spring.config.location=/config/application.yml
+java $JAVA_DEBUG_ARGS -Djava.security.egd=file:/dev/./urandom -jar /opt/app.jar --spring.config.location=/config/application.yml
 
 exec "$@"
