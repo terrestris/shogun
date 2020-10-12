dde1e9f7c9a6c15b64613ef8040dd5bb855b8e68
Lukas Lohoff <lohoff@terrestris.de>
2020-10-12 09:37:04 +0200

    code style

--- shogun-lib/src/main/java/de/terrestris/shogun/lib/controller/BaseController.java
+++ shogun-lib/src/main/java/de/terrestris/shogun/lib/controller/BaseController.java
@@ -55,10 +55,9 @@ public abstract class BaseController<T extends BaseService<?, S>, S extends Base
         log.trace("Requested to return all entities of type {}", getGenericClassName());
 
         try {
-             Page<S> persistedEntities = service.findAll(pageable);
-
-             log.trace("Successfully got all entities of type {} (count: {})",
-                getGenericClassName(), persistedEntities.getTotalElements());
+            Page<S> persistedEntities = service.findAll(pageable);
+            log.trace("Successfully got all entities of type {} (count: {})",
+            getGenericClassName(), persistedEntities.getTotalElements());
 
             return persistedEntities;
         } catch (AccessDeniedException ade) {
