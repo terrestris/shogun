5b1ec7eb3be82b1822b8188db349add21c1bd99e
Daniel Koch <koch@terrestris.de>
2020-10-29 09:50:55 +0100

    Don't make use of deprecated static

--- shogun-lib/src/main/java/de/terrestris/shogun/lib/graphql/GraphQLProvider.java
+++ shogun-lib/src/main/java/de/terrestris/shogun/lib/graphql/GraphQLProvider.java
@@ -6,20 +6,23 @@ import graphql.GraphQL;
 import graphql.scalars.ExtendedScalars;
 import graphql.schema.GraphQLScalarType;
 import graphql.schema.GraphQLSchema;
-import graphql.schema.idl.*;
-import org.apache.commons.codec.Charsets;
+import graphql.schema.idl.RuntimeWiring;
+import graphql.schema.idl.SchemaGenerator;
+import graphql.schema.idl.SchemaParser;
+import graphql.schema.idl.TypeDefinitionRegistry;
+import graphql.schema.idl.TypeRuntimeWiring;
+import java.io.IOException;
+import java.net.URL;
+import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
+import java.util.List;
+import javax.annotation.PostConstruct;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Component;
-
-import javax.annotation.PostConstruct;
-import java.io.IOException;
-import java.net.URL;
-import java.util.ArrayList;
-import java.util.List;
 
 @Component
 public class GraphQLProvider {
@@ -70,7 +73,7 @@ public class GraphQLProvider {
 
     protected String gatherResources() throws IOException {
         URL url = Resources.getResource("graphql/shogun.graphqls");
-        String sdl = Resources.toString(url, Charsets.UTF_8);
+        String sdl = Resources.toString(url, StandardCharsets.UTF_8);
         return sdl;
     }
 
