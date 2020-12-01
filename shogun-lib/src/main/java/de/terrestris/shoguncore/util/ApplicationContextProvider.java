953b8368ac914386976e407636686b048a6a58cc
Daniel Koch <koch@terrestris.de>
2020-12-01 18:45:28 +0100

    Init ApplicationContextProvider

---
+++ shogun-lib/src/main/java/de/terrestris/shoguncore/util/ApplicationContextProvider.java
@@ -0,0 +1,21 @@
+package de.terrestris.shoguncore.util;
+
+import org.springframework.beans.BeansException;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
+import org.springframework.stereotype.Component;
+
+@Component
+public class ApplicationContextProvider implements ApplicationContextAware {
+
+    private static ApplicationContext applicationContext;
+
+    public static ApplicationContext getContext() {
+        return applicationContext;
+    }
+
+    @Override
+    public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
+        this.applicationContext = applicationContext;
+    }
+}
