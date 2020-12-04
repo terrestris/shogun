5cea5072482742bb3844534d3fb5bc52e1475a71
Daniel Koch <koch@terrestris.de>
2020-12-04 13:19:49 +0100

    Adds missing ID resolver classes

---
+++ shogun-lib/src/main/java/de/terrestris/shoguncore/resolver/ApplicationIdResolver.java
@@ -0,0 +1,6 @@
+package de.terrestris.shoguncore.resolver;
+
+import de.terrestris.shoguncore.model.Application;
+import de.terrestris.shoguncore.service.ApplicationService;
+
+public class ApplicationIdResolver extends BaseEntityIdResolver<Application, ApplicationService> { }
---
+++ shogun-lib/src/main/java/de/terrestris/shoguncore/resolver/ImageFileIdResolver.java
@@ -0,0 +1,6 @@
+package de.terrestris.shoguncore.resolver;
+
+import de.terrestris.shoguncore.model.ImageFile;
+import de.terrestris.shoguncore.service.ImageFileService;
+
+public class ImageFileIdResolver extends BaseEntityIdResolver<ImageFile, ImageFileService> { }
