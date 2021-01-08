dbf847f80fb06f45a620a864ed344e3ea807c4b5
Daniel Koch <koch@terrestris.de>
2021-01-08 14:02:47 +0100

    Add findByEntityAndPermissionCollectionType

--- shogun-lib/src/main/java/de/terrestris/shoguncore/repository/security/permission/UserInstancePermissionRepository.java
+++ shogun-lib/src/main/java/de/terrestris/shoguncore/repository/security/permission/UserInstancePermissionRepository.java
@@ -1,15 +1,24 @@
 package de.terrestris.shoguncore.repository.security.permission;
 
+import de.terrestris.shoguncore.enumeration.PermissionCollectionType;
+import de.terrestris.shoguncore.model.BaseEntity;
 import de.terrestris.shoguncore.model.security.permission.UserInstancePermission;
 import de.terrestris.shoguncore.repository.BaseCrudRepository;
-import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
-import org.springframework.stereotype.Repository;
-
 import java.util.List;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
 
 @Repository
 public interface UserInstancePermissionRepository extends BaseCrudRepository<UserInstancePermission, Long>, JpaSpecificationExecutor<UserInstancePermission> {
 
     List<UserInstancePermission> findAllByEntityId(Long entityId);
+
+    @Query("SELECT u FROM userinstancepermissions u LEFT JOIN u.permissions p WHERE u.entity = :entity AND p.name = :permissionCollectionType")
+    List<UserInstancePermission> findByEntityAndPermissionCollectionType(
+        @Param("entity") BaseEntity entity,
+        @Param("permissionCollectionType") PermissionCollectionType permissionCollectionType
+    );
 
 }
