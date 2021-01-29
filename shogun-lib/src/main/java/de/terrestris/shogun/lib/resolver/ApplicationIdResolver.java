88550367ce936c6d779b67f616ee44079ed6c725
Daniel Koch <koch@terrestris.de>
2021-01-29 14:07:55 +0100

    Add missing resolver classes

---
+++ shogun-lib/src/main/java/de/terrestris/shogun/lib/resolver/ApplicationIdResolver.java
@@ -0,0 +1,6 @@
+package de.terrestris.shogun.lib.resolver;
+
+import de.terrestris.shogun.lib.model.Application;
+import de.terrestris.shogun.lib.service.ApplicationService;
+
+public class ApplicationIdResolver extends BaseEntityIdResolver<Application, ApplicationService> { }
---
+++ shogun-lib/src/main/java/de/terrestris/shogun/lib/resolver/UserIdResolver.java
@@ -0,0 +1,6 @@
+package de.terrestris.shogun.lib.resolver;
+
+import de.terrestris.shogun.lib.model.User;
+import de.terrestris.shogun.lib.service.UserService;
+
+public class UserIdResolver extends BaseEntityIdResolver<User, UserService> { }
