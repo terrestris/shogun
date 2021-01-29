636bfa6bf899b6e799a832ccade1616adff74c3c
Daniel Koch <koch@terrestris.de>
2021-01-29 11:52:39 +0100

    Add method deleteAllByUserId() to delete all permissions via user ID

--- shogun-lib/src/main/java/de/terrestris/shogun/lib/repository/security/permission/UserClassPermissionRepository.java
+++ shogun-lib/src/main/java/de/terrestris/shogun/lib/repository/security/permission/UserClassPermissionRepository.java
@@ -5,8 +5,10 @@ import de.terrestris.shogun.lib.repository.BaseCrudRepository;
 import java.util.Optional;
 import javax.persistence.QueryHint;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.jpa.repository.QueryHints;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
@@ -15,5 +17,9 @@ public interface UserClassPermissionRepository extends BaseCrudRepository<UserCl
     @Query("Select ucp from userclasspermissions ucp where ucp.user.id = ?1 and ucp.className = ?2")
     @QueryHints(@QueryHint(name = org.hibernate.annotations.QueryHints.CACHEABLE, value = "true"))
     Optional<UserClassPermission> findByUserIdAndClassName(Long userId, String className);
+
+    @Modifying
+    @Query(value = "DELETE FROM userclasspermissions u WHERE u.user_id=:userId", nativeQuery = true)
+    void deleteAllByUserId(@Param("userId") Long userId);
 
 }
