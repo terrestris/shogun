b9f8958f4d6df9536fc7b1a8fcd90f508910cebb
Daniel Koch <koch@terrestris.de>
2021-01-29 11:51:56 +0100

    Log IDs of handled entities only

--- shogun-lib/src/main/java/de/terrestris/shogun/lib/service/security/permission/PermissionCollectionService.java
+++ shogun-lib/src/main/java/de/terrestris/shogun/lib/service/security/permission/PermissionCollectionService.java
@@ -1,10 +1,9 @@
 package de.terrestris.shogun.lib.service.security.permission;
 
-import de.terrestris.shogun.lib.repository.security.permission.PermissionCollectionRepository;
 import de.terrestris.shogun.lib.model.security.permission.PermissionCollection;
+import de.terrestris.shogun.lib.repository.security.permission.PermissionCollectionRepository;
 import de.terrestris.shogun.lib.service.BaseService;
 import org.springframework.stereotype.Service;
 
 @Service
-public class PermissionCollectionService extends BaseService<PermissionCollectionRepository, PermissionCollection> {
-}
+public class PermissionCollectionService extends BaseService<PermissionCollectionRepository, PermissionCollection> { }
