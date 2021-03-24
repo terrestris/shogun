27fb98aa3b4351e166e924c6b1baf8f3b9ae44f0
Lukas Lohoff <lohoff@terrestris.de>
2021-03-24 10:36:37 +0100

    simplify graphql getType check

--- shogun-lib/src/main/java/de/terrestris/shogun/lib/graphql/resolver/BaseEntityTypeResolver.java
+++ shogun-lib/src/main/java/de/terrestris/shogun/lib/graphql/resolver/BaseEntityTypeResolver.java
@@ -1,6 +1,5 @@
 package de.terrestris.shogun.lib.graphql.resolver;
 
-import de.terrestris.shogun.lib.model.*;
 import graphql.TypeResolutionEnvironment;
 import graphql.schema.GraphQLObjectType;
 import graphql.schema.TypeResolver;
@@ -12,22 +11,8 @@ public class BaseEntityTypeResolver implements TypeResolver {
     @Override
     public GraphQLObjectType getType(TypeResolutionEnvironment env) {
         Object javaObject = env.getObject();
-        if (javaObject instanceof Application) {
-            return env.getSchema().getObjectType("Application");
-        } else if (javaObject instanceof User) {
-            return env.getSchema().getObjectType("User");
-        } else if (javaObject instanceof ImageFile) {
-            return env.getSchema().getObjectType("ImageFile");
-        } else if (javaObject instanceof File) {
-            return env.getSchema().getObjectType("File");
-        } else if (javaObject instanceof Group) {
-            return env.getSchema().getObjectType("Group");
-        } else if (javaObject instanceof Layer) {
-            return env.getSchema().getObjectType("Layer");
-        } else {
-            log.warn("Getting object type from class name {}", javaObject.getClass().getSimpleName());
-            return env.getSchema().getObjectType(javaObject.getClass().getSimpleName());
-        }
+        log.trace("Getting object type from class name {}", javaObject.getClass().getSimpleName());
+        return env.getSchema().getObjectType(javaObject.getClass().getSimpleName());
     }
 
 }
