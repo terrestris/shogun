9ce80305af4752bd72f311db0d763a0415052592
Daniel Koch <koch@terrestris.de>
2021-07-15 14:46:43 +0200

    Add migration to remove field i18n from application

---
+++ shogun-boot/src/main/resources/db/migration/V0.9.0__Remove_i18n_field_from_Application.sql
@@ -0,0 +1,3 @@
+ALTER TABLE IF EXISTS shogun.applications DROP COLUMN IF EXISTS i18n;
+ALTER TABLE IF EXISTS shogun_rev.applications_rev DROP COLUMN IF EXISTS i18n;
+ALTER TABLE IF EXISTS  shogun_rev.applications_rev DROP COLUMN IF EXISTS i18n_mod;
