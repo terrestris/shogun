1a07b663b9e7145be650baee143867cb4d79b930
Andreas Schmitz <schmitz@terrestris.de>
2021-10-04 12:34:01 +0200

    Integrate admin client

---
+++ shogun-boot/src/main/java/de/terrestris/shogun/boot/controller/ResourceController.java
@@ -0,0 +1,38 @@
+/* SHOGun, https://terrestris.github.io/shogun/
+ *
+ * Copyright © 2020-present terrestris GmbH & Co. KG
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   https://www.apache.org/licenses/LICENSE-2.0.txt
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package de.terrestris.shogun.boot.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class ResourceController {
+
+    @Value("${KEYCLOAK_HOST}")
+    String keycloakHost;
+
+    @GetMapping("/admin/client-config.js")
+    public ModelAndView getAdminClientConfig(ModelAndView modelAndView) {
+        modelAndView.addObject("KEYCLOAK_HOST", keycloakHost);
+        modelAndView.setViewName("client-config.js");
+
+        return modelAndView;
+    }
+
+}
---
+++ shogun-boot/src/main/resources/templates/client-config.js
@@ -0,0 +1,101 @@
+//  SHOGun, https://terrestris.github.io/shogun/
+//
+//  Copyright © 2020-present terrestris GmbH & Co. KG
+//
+//  Licensed under the Apache License, Version 2.0 (the "License");
+//  you may not use this file except in compliance with the License.
+//  You may obtain a copy of the License at
+//
+//    https://www.apache.org/licenses/LICENSE-2.0.txt
+//
+//  Unless required by applicable law or agreed to in writing, software
+//  distributed under the License is distributed on an "AS IS" BASIS,
+//  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//  See the License for the specific language governing permissions and
+//  limitations under the License.
+var shogunApplicationConfig = {
+  appPrefix: '/admin',
+  path: {
+    base: 'https://localhost',
+    swagger: '/v2/api-docs',
+    user: '/users',
+    layer: '/layers',
+    imageFile: '/imagefiles',
+    application: '/applications',
+    appInfo: '/info/app',
+    auth: {
+      login: '/auth/login',
+      logout: '/auth/logout',
+      isSessionValid: '/auth/isSessionValid'
+    },
+    keycloak: {
+      base: 'https://[(${KEYCLOAK_HOST})]/auth',
+      realm: 'SpringBootKeycloak',
+      clientId: 'shogun-app'
+    },
+    loggers: '/actuator/loggers',
+    logfile: '/actuator/logfile',
+    logo: '/static/img/shogun_logo.ed603078.png',
+    evictCache: '/cache/evict',
+    metrics: '/actuator/metrics'
+  },
+  dashboard: {
+    news: {
+      visible: false
+    },
+    statistics: {
+      visible: false
+    },
+    applications: {
+      visible: true
+    },
+    layers: {
+      visible: true
+    },
+    users: {
+      visible: true
+    }
+  },
+  navigation: {
+    general: {
+      applications: {
+        visible: true,
+        schemas: {
+          clientConfig: 'IhkApplicationClientConfig',
+          layerTree: 'SHOGunLayerTree',
+          layerConfig: 'LayerConfig'
+        }
+      },
+      layers: {
+        visible: true,
+        schemas: {
+          clientConfig: 'SHOGunLayerClientConfig',
+          sourceConfig: 'SHOGunLayerSourceConfig',
+          features: 'GeoJsonObject'
+        }
+      },
+      users: {
+        visible: true
+      },
+      imagefiles: {
+        visible: false
+      }
+    },
+    status: {
+      metrics: {
+        visible: true
+      },
+      logs: {
+        visible: true
+      }
+    },
+    settings: {
+      global: {
+        visible: true
+      },
+      logs: {
+        visible: true
+      }
+    }
+  }
+};
