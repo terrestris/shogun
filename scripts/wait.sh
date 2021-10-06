b4193deb0c1df94ba791a74c2843c1c6859b237b
Lukas Lohoff <lohoff@terrestris.de>
2021-10-06 10:27:53 +0200

    fix: specify mvn output and fix wait check

--- scripts/wait.sh
+++ scripts/wait.sh
@@ -14,7 +14,7 @@
 #  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 #  See the License for the specific language governing permissions and
 #  limitations under the License.
-URL="https://localhost/"
+URL="http://localhost:8080/"
 TIMEOUT=120
 seconds=0
 
