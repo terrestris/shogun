cd8c0606602c64b916fe32dd4ea71d5368724983
Daniel Koch <koch@terrestris.de>
2021-10-16 15:52:51 +0300

    Apply the given schema to the native query

--- shogun-lib/src/main/java/de/terrestris/shogun/lib/repository/security/permission/UserClassPermissionRepository.java
+++ shogun-lib/src/main/java/de/terrestris/shogun/lib/repository/security/permission/UserClassPermissionRepository.java
@@ -19,15 +19,16 @@ package de.terrestris.shogun.lib.repository.security.permission;
 import de.terrestris.shogun.lib.model.User;
 import de.terrestris.shogun.lib.model.security.permission.UserClassPermission;
 import de.terrestris.shogun.lib.repository.BaseCrudRepository;
-import java.util.List;
-import java.util.Optional;
-import javax.persistence.QueryHint;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.jpa.repository.QueryHints;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import javax.persistence.QueryHint;
+import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface UserClassPermissionRepository extends BaseCrudRepository<UserClassPermission, Long>, JpaSpecificationExecutor<UserClassPermission> {
@@ -40,6 +41,6 @@ public interface UserClassPermissionRepository extends BaseCrudRepository<UserCl
     List<UserClassPermission> findAllByUser(User user);
 
     @Modifying
-    @Query(value = "DELETE FROM userclasspermissions u WHERE u.user_id=:userId", nativeQuery = true)
+    @Query(value = "DELETE FROM {h-schema}userclasspermissions u WHERE u.user_id=:userId", nativeQuery = true)
     void deleteAllByUserId(@Param("userId") Long userId);
 }
