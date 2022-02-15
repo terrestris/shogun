3018ad6bf920b203543df9f74cb481a0e3d34079
Daniel Koch <koch@terrestris.de>
2022-02-15 11:25:14 +0100

    Include default JSONB (de-)serializers in jackson and swagger config and make them overwritable/extentable

--- shogun-lib/src/main/java/de/terrestris/shogun/lib/annotation/JsonSuperType.java
+++ shogun-lib/src/main/java/de/terrestris/shogun/lib/annotation/JsonSuperType.java
@@ -26,7 +26,8 @@ import java.lang.annotation.Target;
  * The `JsonSuperType` annotation helps to identify a type that should be deserialized instead of a parent type.
  * The `type` property specifies the parent type. This annotation needs to go along with a `JsonDeserialize` pointing
  * to the own type.
- * If the super type that should get replaced is not an interface, than `override` needs to be set to `true`.
+ * If the super type that should get replaced is already used by another parent, and it should be overridden by another
+ * type, then `override` needs to be set to `true`.
  */
 @Retention(RetentionPolicy.RUNTIME)
 @Target(ElementType.TYPE)
