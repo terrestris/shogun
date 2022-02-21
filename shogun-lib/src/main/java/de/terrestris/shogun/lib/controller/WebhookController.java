026cc126be97d41532746a45b538c9137b8998ec
Simon Seyock <seyock@terrestris.de>
2022-02-21 16:33:49 +0100

    feat: keycloak application event

--- shogun-lib/src/main/java/de/terrestris/shogun/lib/controller/WebhookController.java
+++ shogun-lib/src/main/java/de/terrestris/shogun/lib/controller/WebhookController.java
@@ -17,38 +17,33 @@
 package de.terrestris.shogun.lib.controller;
 
 import de.terrestris.shogun.lib.dto.KeycloakEventDto;
-import de.terrestris.shogun.lib.service.GroupService;
-import de.terrestris.shogun.lib.service.UserService;
+import de.terrestris.shogun.lib.event.KeycloakEvent;
+import de.terrestris.shogun.lib.event.KeycloakEventType;
 import lombok.extern.log4j.Log4j2;
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.Arrays;
-import java.util.HashSet;
 import java.util.Set;
 
 @Log4j2
 @RestController
 @RequestMapping("/webhooks")
 public class WebhookController {
-
     @Autowired
-    UserService userService;
-
-    @Autowired
-    GroupService groupService;
+    private ApplicationEventPublisher applicationEventPublisher;
 
     @PostMapping(value = "/keycloak")
     public void handleKeyCloakEvent(@RequestBody KeycloakEventDto event) {
-        Set<String> relevantResourceTypes = new HashSet(Arrays.asList(
+        Set<String> relevantResourceTypes = Set.of(
             "GROUP_MEMBERSHIP",
             "GROUP",
             "USER"
-        ));
+        );
 
         log.debug("Keycloak webhook called with event: {}", event);
         String resourceType = event.getResourceType();
@@ -58,18 +53,38 @@ public class WebhookController {
             if (StringUtils.isNotEmpty(resourcePath)) {
                 String[] split = resourcePath.split("/");
                 if (StringUtils.equals(resourceType, "GROUP_MEMBERSHIP")) {
-                    userService.findOrCreateByKeyCloakId(split[1]);
+                    applicationEventPublisher.publishEvent(new KeycloakEvent(
+                        this,
+                        KeycloakEventType.USER_GROUP_MEMBERSHIP_CHANGED,
+                        split[1]
+                    ));
                 } else if (StringUtils.equals(resourceType, "USER")) {
                     if (StringUtils.equals(eventType, "CREATE")) {
-                        userService.findOrCreateByKeyCloakId(split[1]);
+                        applicationEventPublisher.publishEvent(new KeycloakEvent(
+                            this,
+                            KeycloakEventType.USER_CREATED,
+                            split[1]
+                        ));
                     } else if (StringUtils.equals(eventType, "DELETE")) {
-                        userService.deleteByKeycloakId(split[1]);
+                        applicationEventPublisher.publishEvent(new KeycloakEvent(
+                            this,
+                            KeycloakEventType.USER_DELETED,
+                            split[1]
+                        ));
                     }
                 } else if (StringUtils.equals(resourceType, "GROUP")) {
                     if (StringUtils.equals(eventType, "CREATE")) {
-                        groupService.findOrCreateByKeycloakId(split[1]);
+                        applicationEventPublisher.publishEvent(new KeycloakEvent(
+                            this,
+                            KeycloakEventType.GROUP_CREATED,
+                            split[1]
+                        ));
                     } else if (StringUtils.equals(eventType, "DELETE")) {
-                        groupService.deleteByKeycloakId(split[1]);
+                        applicationEventPublisher.publishEvent(new KeycloakEvent(
+                            this,
+                            KeycloakEventType.GROUP_DELETED,
+                            split[1]
+                        ));
                     }
                 }
             }
