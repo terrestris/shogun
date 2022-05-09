af3e0882b3a4e9e57e56dbee09510aad27ba4197
Kai Volland <volland@terrestris.de>
2022-05-09 14:41:28 +0200

    remove ApplicationPermissionEvaluator

--- shogun-lib/src/main/java/de/terrestris/shogun/lib/model/jsonb/ApplicationClientConfig.java
+++ shogun-lib/src/main/java/de/terrestris/shogun/lib/model/jsonb/ApplicationClientConfig.java
@@ -17,9 +17,6 @@
 package de.terrestris.shogun.lib.model.jsonb;
 
 import java.io.Serializable;
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
 
 public interface ApplicationClientConfig extends Serializable {
 }
--- shogun-lib/src/test/java/de/terrestris/shogun/lib/security/access/BasePermissionEvaluatorTest.java
+++ shogun-lib/src/test/java/de/terrestris/shogun/lib/security/access/BasePermissionEvaluatorTest.java
@@ -20,7 +20,6 @@ import de.terrestris.shogun.lib.enumeration.PermissionType;
 import de.terrestris.shogun.lib.model.Application;
 import de.terrestris.shogun.lib.model.BaseEntity;
 import de.terrestris.shogun.lib.model.User;
-import de.terrestris.shogun.lib.security.access.entity.ApplicationPermissionEvaluator;
 import de.terrestris.shogun.lib.security.access.entity.BaseEntityPermissionEvaluator;
 import de.terrestris.shogun.lib.security.access.entity.DefaultPermissionEvaluator;
 import de.terrestris.shogun.lib.service.security.provider.UserProviderService;
@@ -48,9 +47,6 @@ public class BasePermissionEvaluatorTest {
     private DefaultPermissionEvaluator defaultPermissionEvaluatorMock;
 
     @Mock
-    private ApplicationPermissionEvaluator applicationPermissionEvaluatorMock;
-
-    @Mock
     private UserProviderService userProviderService = new KeycloakUserProviderService();
 
     @Spy
@@ -68,7 +64,6 @@ public class BasePermissionEvaluatorTest {
         mockUser.setAuthProviderId(mockUserKeycloakId);
 
         when(defaultPermissionEvaluatorMock.getEntityClassName()).thenReturn(BaseEntity.class);
-        when(applicationPermissionEvaluatorMock.getEntityClassName()).thenReturn(Application.class);
     }
 
     @Test
@@ -166,12 +161,10 @@ public class BasePermissionEvaluatorTest {
         when(userProviderService.getUserFromAuthentication(authentication)).thenReturn(Optional.of(mockUser));
 
         baseEntityPermissionEvaluatorMock.add(defaultPermissionEvaluatorMock);
-        baseEntityPermissionEvaluatorMock.add(applicationPermissionEvaluatorMock);
 
         permissionEvaluator.hasPermission(authentication, targetDomainObject, permissionObject);
 
         verify(defaultPermissionEvaluatorMock, times(0)).hasPermission(mockUser, targetDomainObject, PermissionType.READ);
-        verify(applicationPermissionEvaluatorMock, times(1)).hasPermission(mockUser, targetDomainObject, PermissionType.READ);
 
         reset(userProviderService);
     }
