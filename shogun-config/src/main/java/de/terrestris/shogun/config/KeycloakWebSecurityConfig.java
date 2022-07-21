a259f06a3206458a9b15caf1056650972576d7a8
Lukas Lohoff <lohoff@terrestris.de>
2022-07-21 15:28:19 +0200

    fix: optimize imports

--- shogun-config/src/main/java/de/terrestris/shogun/config/KeycloakWebSecurityConfig.java
+++ shogun-config/src/main/java/de/terrestris/shogun/config/KeycloakWebSecurityConfig.java
@@ -25,8 +25,6 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
 
 import javax.annotation.PostConstruct;
 import javax.net.ssl.HttpsURLConnection;
