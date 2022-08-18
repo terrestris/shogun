a81aa19c736f59a157f0ac8b6530c57f71c12c03
Daniel Koch <koch@terrestris.de>
2022-08-18 09:32:20 +0200

    Remove extra whitespaces

--- shogun-lib/src/main/java/de/terrestris/shogun/lib/exception/security/permission/ReadPermissionException.java
+++ shogun-lib/src/main/java/de/terrestris/shogun/lib/exception/security/permission/ReadPermissionException.java
@@ -3,8 +3,8 @@
  *
  * Copyright © 2022-present terrestris GmbH & Co. KG
  *
- *  Licensed under the Apache License, Version 2.0 (the "License");
- *  you may not use this file except in compliance with the License.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
  *   https://www.apache.org/licenses/LICENSE-2.0.txt
