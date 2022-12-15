8cd963786941a1865d3f5280756b73619b896f87
Lukas Lohoff <lohoff@terrestris.de>
2022-12-15 17:14:01 +0100

    fix: reenable graphql-java-extended-scalars

--- shogun-lib/src/main/java/de/terrestris/shogun/lib/config/GraphQLConfig.java
+++ shogun-lib/src/main/java/de/terrestris/shogun/lib/config/GraphQLConfig.java
@@ -16,10 +16,8 @@
  */
 package de.terrestris.shogun.lib.config;
 
-import de.terrestris.shogun.lib.graphql.scalar.DateTimeScalar;
 import de.terrestris.shogun.lib.graphql.scalar.GeometryScalar;
-import de.terrestris.shogun.lib.graphql.scalar.JsonScalar;
-import de.terrestris.shogun.lib.graphql.scalar.LongScalar;
+import graphql.scalars.ExtendedScalars;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.graphql.execution.RuntimeWiringConfigurer;
@@ -30,10 +28,10 @@ public class GraphQLConfig {
     @Bean
     public RuntimeWiringConfigurer runtimeWiringConfigurer() {
         return wiringBuilder -> {
-            wiringBuilder.scalar(JsonScalar.INSTANCE);
+            wiringBuilder.scalar(ExtendedScalars.Json);
             wiringBuilder.scalar(GeometryScalar.INSTANCE);
-            wiringBuilder.scalar(DateTimeScalar.INSTANCE);
-            wiringBuilder.scalar(LongScalar.INSTANCE);
+            wiringBuilder.scalar(ExtendedScalars.DateTime);
+            wiringBuilder.scalar(ExtendedScalars.GraphQLLong);
         };
     }
 
