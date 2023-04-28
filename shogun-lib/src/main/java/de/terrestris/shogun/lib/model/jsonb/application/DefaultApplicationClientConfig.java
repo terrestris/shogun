c85dc968ffd5562ef92dad59f77872d869a6fdf1
hblitza <37144910+hblitza@users.noreply.github.com>
2023-04-28 13:34:32 +0200

    feat: add defaultLanguage param to clientConfig (#685)

--- shogun-lib/src/main/java/de/terrestris/shogun/lib/model/jsonb/application/DefaultApplicationClientConfig.java
+++ shogun-lib/src/main/java/de/terrestris/shogun/lib/model/jsonb/application/DefaultApplicationClientConfig.java
@@ -54,4 +54,9 @@ public class DefaultApplicationClientConfig implements ApplicationClientConfig {
     )
     private DefaultApplicationTheme theme;
 
+    @Schema(
+        description = "The default language of the application (ISO 639-1)."
+    )
+    private String defaultLanguage;
+
 }
